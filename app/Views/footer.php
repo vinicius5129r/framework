fdc1eb5b71767f19b846b6395215af2052b58afa
JOSE VINICIUS SOARES RAMOS <viniciusramos5129r@gmail.com>
2026-07-01 08:57:58 -0300

    Improve header/footer and add about card + image
    Fix header markup and navbar (use URL, bootstrap collapse, add Home/Sobre links and right-aligned action buttons). Update footer to use APP_VERSAO constant. Enhance about page (paginas/sobre) with a Bootstrap card that displays an added image asset and show APP_VERSAO. Add public/img/gato.jpg image asset.

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -1,7 +1,7 @@
 <footer class= "bg-dark p-5 text-light">
     <div class="container">
         <small>
-            Aula de PHP Orientado a Objetos e MVC. Versão <?= APP_VERSION ?>
+            Aula de PHP Orientado a Objetos e MVC. Versão <?= APP_VERSAO ?>
             <div class= "border-top mt-3">
                 &COPY; 2026 - <?= date('Y') ?> IFRO - 
                 Guajará-Mirim / Informáica - Programação Web
--- app/Views/paginas/sobre.php
+++ app/Views/paginas/sobre.php
@@ -1,3 +1,12 @@
 <h1><?php echo $dados['titulo']; ?></h1>
 <p><?php echo $dados['descricao']; ?></p>
+
+<div class="card" style="width: 18rem;">
+    <img src="<?= URL ?>/public/img/gato.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">GATO PIRULETA</h5>
+        <p class="card-text">Um gato muito fofinho e brincalhão, ele adora brincar com bolinhas de lã e dormir em lugares quentes.</p>
+        <a href="https://ehgomes.com.br/software/vectorizer-ai-a-ferramenta-que-transforma-imagens-em-vetores/" class="btn btn-primary">Ver mais...</a>
+    </div>
+</div>
 <?= APP_VERSAO ?>
